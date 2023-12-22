bc4b77c27c7e0a6481ebc04b0ddaf82f0bebb017
Asief Mahir <asiefmahir1@gmail.com>
2023-12-22 23:32:04 +0600

    feat: Create a Note Taking App with CRUD Operations
    Create a Note Taking App With Create, Read, Update and Delete Functionalities.

--- src/App.js
+++ src/App.js
@@ -1,81 +1,100 @@
-import BioData from "./components/BioData";
+import { useState } from "react";
 import "./App.css";
 
-// const obj = {
-// 	a: 10,
-// 	b: 20,
-// 	c: 30,
-// };
+const App = () => {
+	// State Lifting
+	// states
+	const [noteTitle, setNoteTitle] = useState("");
+	const [notes, setNotes] = useState([]);
+	const [editMode, setEditMode] = useState(false);
+	const [editableNote, setEditableNote] = useState(null);
 
-const bioInfos = [
-	{
-		id: 1,
-		name: "Mahir Asief",
-		age: "26",
-		email: "asiefmahir1@gmail.com",
-		skills: ["Js", "TS", "React", "Node"],
-		interests: ["System Design", "Chess", "Cricket"],
-	},
-	{
-		id: 2,
-		name: "Ahmed Shahin",
-		age: "25",
-		phone: 354354354354,
-		email: "sahih@gmail.com",
-		skills: ["Js", "TS", "React", "Node", "html", "css"],
-		interests: ["System Design", "Football"],
-	},
-];
+	const submitHandler = (event) => {
+		event.preventDefault();
+		editMode ? updateHandler() : createHandler();
 
-function App() {
+		// notes.push(note);
+	};
+
+	const createHandler = () => {
+		if (!noteTitle) {
+			return alert(`Please provide a valid note title`);
+		}
+		console.log(noteTitle);
+
+		const note = {
+			id: Date.now() + "",
+			title: noteTitle,
+		};
+
+		setNotes([...notes, note]);
+		setNoteTitle("");
+	};
+
+	const removeHandler = (noteId) => {
+		// noteId = 2
+		// notes.splice()
+		const updatedNotes = notes.filter((note) => note.id !== noteId);
+		// 								()		=> 1 !== 2
+		//								()      => 2 !== 2
+
+		setNotes(updatedNotes);
+		// notes = updatedNotes
+	};
+
+	const editHandler = (note) => {
+		setEditMode(true);
+		//editMode = true
+		setEditableNote(note);
+		// editableNote = note
+		setNoteTitle(note.title);
+	};
+
+	const updateHandler = () => {
+		const updatedNotes = notes.map((note) => {
+			if (note.id === editableNote.id) {
+				return { ...note, title: noteTitle };
+			}
+
+			return note;
+		});
+
+		console.log(updatedNotes);
+		setNotes(updatedNotes);
+		setEditMode(false);
+		setEditableNote(null);
+		setNoteTitle("");
+	};
 	return (
 		<div className="App">
-			{bioInfos.map((biodata) => (
-				<BioData
-					key={biodata.id}
-					name={biodata.name}
-					age={biodata.age}
-					email={biodata.email}
-					skills={biodata.skills}
-					interests={biodata.interests}
-					phone={biodata.phone}
+			<form onSubmit={submitHandler}>
+				<input
+					type="text"
+					value={noteTitle}
+					onChange={(event) => setNoteTitle(event.target.value)}
 				/>
-			))}
+				<button type="submit">
+					{editMode ? "Update Note" : "Add Note"}
+				</button>
+			</form>
+			<div className="notes">
+				<h2>All Notes</h2>
+				<ul className="note-list">
+					{notes.map((note) => (
+						<li key={note.id}>
+							<span>{note.title}</span>
+							<button onClick={() => editHandler(note)}>
+								Edit
+							</button>
+							<button onClick={() => removeHandler(note.id)}>
+								Remove
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
-	// console.log("I am being called");
-	// return (
-	// 	<div className="App" id="App">
-	// 		<BioData
-	// 			name="Mahir Asief"
-	// 			age="26"
-	// 			email="asiefmahir1@gmail.com"
-	// 			skills={["Js", "TS", "React", "Node"]}
-	// 			interests={["System Design", "Chess", "Cricket"]}
-	// 		/>
-	// 		<hr />
-	// 		<hr />
-	// 		<BioData
-	// 			name="Ahmed Shahin"
-	// 			age="25"
-	// 			email="shahin@gmail.com"
-	// 			phone="4534545845641"
-	// 			skills={["Js", "TS", "React", "Node", "html", "css"]}
-	// 			interests={["System Design", "Football"]}
-	// 		/>
-	// 	</div>
-	// );
-}
-
-// App()
-
-/**
- * Component must have 3 Criteria:
- * 1. Must be a function
- * 2. That function should return "something"
- * 3. That "something" must be some html-ish code (JSX)
- */
-
-// App()
+};
 
 export default App;
