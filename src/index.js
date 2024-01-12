3ea29d3b54bd5b9f2031f6a65786f731ead6f9c5
Asief Mahir <asiefmahir1@gmail.com>
2024-01-13 00:12:24 +0600

    feat: Learn about the fundamentals of React Router

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import StudentProvider from "./contexts/Student";
+import { RouterProvider } from "react-router-dom";
+import { ourRouter } from "./router/router";
+// import App from "./App";
+// import StudentProvider from "./contexts/Student";
 
-import reportWebVitals from "./reportWebVitals";
-import App2 from "./App2";
+// import reportWebVitals from "./reportWebVitals";
+// import App2 from "./App2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<StudentProvider>
+		{/* <StudentProvider>
 			<App />
-		</StudentProvider>
+		</StudentProvider> */}
 		{/* <App2 />
 		<App /> */}
+		<RouterProvider router={ourRouter} />
 	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
