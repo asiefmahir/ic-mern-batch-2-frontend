e6d3ca3504f93dea8eaf0e730d5b6feaa1e651aa
Asief Mahir <asiefmahir1@gmail.com>
2024-01-20 06:40:06 +0600

    feat: Add the Drag & Drop Feature and it's corresponding logic

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-
+import { Droppable } from "react-beautiful-dnd";
 import { icons } from "../assets";
 
 import AddItem from "./AddItem";
@@ -8,7 +8,7 @@ import AddItemForm from "./AddItemForm";
 import { BoardContext } from "../contexts/Board";
 import { ListContext } from "../contexts/List";
 import { TaskContext } from "../contexts/Task";
-// import TaskCard from "./TaskCard";
+import TaskCard from "./TaskCard";
 
 const TaskList = ({ taskList }) => {
 	const [editMode, setEditMode] = useState(false);
@@ -59,37 +59,53 @@ const TaskList = ({ taskList }) => {
 	};
 
 	return (
-		<div className="list-container">
-			<div className="list-title-container">
-				<h5>{taskList.title}</h5>
-				<img
-					onClick={removeHandler}
-					src={icons.crossIcon}
-					alt=""
-					className="add-item-icon"
-				/>
-			</div>
-			{taskList.tasks
-				.map((item) => {
-					return allTasks.find((ele) => ele.id === item);
-				})
-				.map((task) => (
-					// <TaskCard key = {task.id} task={task}/>
-					<p key={task.id}>{task.title}</p>
-				))}
-			{!editMode ? (
-				<AddItem setEditMode={setEditMode} />
-			) : (
-				<AddItemForm
-					title={taskTitle}
-					onChangeHandler={(e) => {
-						setTaskTitle(e.target.value);
-					}}
-					setEditMode={setEditMode}
-					submitHandler={submitHandler}
-				/>
-			)}
-		</div>
+		<Droppable droppableId={taskList.id}>
+			{(provided) => {
+				// console.log(provided, "params from dropabble");
+				return (
+					<div
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+						className="list-container"
+					>
+						<div className="list-title-container">
+							<h5>{taskList.title}</h5>
+							<img
+								onClick={removeHandler}
+								src={icons.crossIcon}
+								alt=""
+								className="add-item-icon"
+							/>
+						</div>
+						{taskList.tasks
+							.map((item) => {
+								return allTasks.find((ele) => ele.id === item);
+							})
+							.map((task, index) => (
+								<TaskCard
+									index={index}
+									key={task.id}
+									task={task}
+								/>
+								// <p key={task.id}>{task.title}</p>
+							))}
+						{!editMode ? (
+							<AddItem setEditMode={setEditMode} />
+						) : (
+							<AddItemForm
+								title={taskTitle}
+								onChangeHandler={(e) => {
+									setTaskTitle(e.target.value);
+								}}
+								setEditMode={setEditMode}
+								submitHandler={submitHandler}
+							/>
+						)}
+						{provided.placeholder}
+					</div>
+				);
+			}}
+		</Droppable>
 	);
 };
 
